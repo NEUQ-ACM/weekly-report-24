32c8497ee6edba8188eaf5ca1ff9368d919d4b8e
Stream <1542763342@qq.com>
2024-06-16 06:47:17 +0800

    本周项目

---
+++ 万荣涛/week2/JdbcAndSwingTest/src/main/java/pers/stream/jdbcandswingtest/Main.java
@@ -0,0 +1,13 @@
+package pers.stream.jdbcandswingtest;
+
+import lombok.val;
+import pers.stream.jdbcandswingtest.ui.CommandUi;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        val ui = new CommandUi();
+
+    }
+}
